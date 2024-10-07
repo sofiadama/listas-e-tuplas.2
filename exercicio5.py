5d7b80c5297e9850813efcfeece28e17cf157066
SOFIA <sofiadamasin@gmail.com>
2024-10-07 18:44:07 -0300

    Update exercicio5.py

--- exercicio5.py
+++ exercicio5.py
@@ -1,11 +1,11 @@
-1743
 X = list(map(int,input().split()))[:5]
 Y = list(map(int,input().split()))[:5]
 
 for i in range(len(X)):
-     if X[i] > 1 and Y[i] > 1:
-           print(‘Valores inválidos!’)
+     if X[i] > 1 or Y[i] > 1:
+           print('Valores inválidos!')
      elif X[i] != Y[i]:
-           print(‘S’)
+           print('S')
      else: 
-           print(‘N’)
+           print('N')
+     break
