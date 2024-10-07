6a0a76ab9e381e820390278deb1a52e67b021a6d
SOFIA <sofiadamasin@gmail.com>
2024-10-07 18:50:09 -0300

    Update exercicio6.py

--- exercicio6.py
+++ exercicio6.py
@@ -1,9 +1,6 @@
-1172
-
 X = list(map(int,input().split()))[:10]
 
 for i in range(len(X)):
-     if X[i] < 1:
-          X[i] = 1 
-
-print(f‘[X{i}] = {X[i]}’) 
+    if X[i] < 1:
+        X[i] = 1 
+    print(f'{i+1}º = {X[i]}') 
