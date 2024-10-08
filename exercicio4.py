34fe027c277ddde94ee5ed2fe250797e5a0fac51
SOFIA <sofiadamasin@gmail.com>
2024-10-08 11:44:42 -0300

    Update exercicio4.py

--- exercicio4.py
+++ exercicio4.py
@@ -1,12 +1,17 @@
-import math
 testes = int(input('Testes: '))
 
 for i in range(testes):
-    A,R = list(map(int,input().split()))
-    tentativas = list(map(int,input('Número: ').split()))[:A]
-    
-    R = float(R)
-    for n in tentativas:
-        if n == R or n == math.floor(R):
-            posicao = tentativas.index(n)
-            print(f'{posicao+1}º aluno correto!')
+    A,R = list(map(int,input('Alunos e resposta: ').split()))
+    tentativas = list(map(int,input('Tentativas: ').split()))[:A]
+   
+    found = False
+    if R in tentativas:
+        posicao = tentativas.index(R)
+        print(f'{posicao+1}º aluno correto!')
+        found = True
+        
+    else:
+        proximo = [(abs(R - n), i) for i, n in enumerate(tentativas)]
+        proximo.sort()
+        posicao = proximo[0][1]
+        print(f'{posicao+1}º aluno correto!')
